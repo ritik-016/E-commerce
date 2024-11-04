6d72554566585b80a7dc3c7a0f19fa8ed9a67dfa
Ritik Kumar <ad6360153@gmail.com>
2024-11-04 08:57:06 +0530

    Updates

--- script.js
+++ script.js
@@ -28,3 +28,19 @@ SmallImg[2].onclick = function(){
 SmallImg[3].onclick = function(){
     PrductImg.src = SmallImg[3].src;
 }
+
+// js for toggle form 
+var loginform = document.getElementById("loginform");
+      var regform = document.getElementById("regform");
+      var indicator = document.getElementById("indicator");
+
+      function register() {
+        regform.style.transform = "translateX(0px)";
+        loginform.style.transform = "translateX(0px)";
+        indicator.style.transform = "translateX(100px)";
+      }
+      function login() {
+        regform.style.transform = "translateX(300px)";
+        loginform.style.transform = "translateX(300px)";
+        indicator.style.transform = "translateX(0px)";
+      }
